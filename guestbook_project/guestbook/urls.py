cd5694a1af1f8a12eb407e36ac68ba1205841788
aibekov_nurkerim <aibekovnurkerim@gmail.com>
2024-07-06 16:10:21 +0600

    Реализовал функцию редактирования

--- guestbook_project/guestbook/urls.py
+++ guestbook_project/guestbook/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.guestbook_home, name='guestbook_home'),
     path('add/', views.add_guestbook_entry, name='add_guestbook_entry'),
+    path('edit/<int:pk>/', views.edit_guestbook_entry, name='edit_guestbook_entry'),
 ]
--- guestbook_project/guestbook/views.py
+++ guestbook_project/guestbook/views.py
@@ -1,6 +1,8 @@
 from .models import GuestbookEntry
 from django.shortcuts import render, redirect
 from .forms import GuestbookEntryForm
+from django.shortcuts import get_object_or_404
+
 def guestbook_home(request):
     entries = GuestbookEntry.objects.filter(status='active').order_by('-created_at')
     return render(request, 'guestbook/home.html', {'entries': entries})
@@ -15,3 +17,15 @@ def add_guestbook_entry(request):
     else:
         form = GuestbookEntryForm()
     return render(request, 'guestbook/add_entry.html', {'form': form})
+
+def edit_guestbook_entry(request, pk):
+    entry = get_object_or_404(GuestbookEntry, pk=pk)
+    if request.method == 'POST':
+        form = GuestbookEntryForm(request.POST, instance=entry)
+        if form.is_valid():
+            form.save()
+            return redirect('guestbook_home')
+    else:
+        form = GuestbookEntryForm(instance=entry)
+    return render(request, 'guestbook/edit_entry.html', {'form': form, 'entry': entry})
+
