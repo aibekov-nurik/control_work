d7f2ed0b80c055ae69b096f540da7bfc016ab12f
aibekov_nurkerim <aibekovnurkerim@gmail.com>
2024-07-06 16:03:00 +0600

    Создал модель гостевой книги

--- guestbook_project/guestbook/admin.py
+++ guestbook_project/guestbook/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import GuestbookEntry
 
-# Register your models here.
+@admin.register(GuestbookEntry)
+class GuestbookEntryAdmin(admin.ModelAdmin):
+    list_display = ('author_name', 'author_email', 'entry_text', 'created_at', 'updated_at', 'status')
+    list_filter = ('status', 'created_at')
+    search_fields = ('author_name', 'author_email', 'entry_text')
--- guestbook_project/guestbook/models.py
+++ guestbook_project/guestbook/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class GuestbookEntry(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Активно'),
+        ('blocked', 'Заблокировано'),
+    ]
+
+    author_name = models.CharField(max_length=255, verbose_name='Имя автора', blank=False)
+    author_email = models.EmailField(verbose_name='Почта автора', blank=False)
+    entry_text = models.TextField(verbose_name='Текст записи', blank=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время редактирования')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES, default='active', verbose_name='Статус', blank=False)
+
+    def __str__(self):
+        return f'{self.author_name} ({self.get_status_display()})'
